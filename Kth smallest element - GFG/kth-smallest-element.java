da1e88f3dffdc798aff38f8f3502e7a658e0e1d1
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-06-05 22:24:23 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+          int []ans=removeDuplicates(arr);
+        Arrays.sort(ans);
+        return ans[k - 1];
+    }
+    public static int[] removeDuplicates(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        int[] uniqueArr = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            uniqueArr[index++] = num;
+        }
+        return uniqueArr;
+    }
+}
