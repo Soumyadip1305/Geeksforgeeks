419ba0c31fdc5f1f7b4d38b970615764bdfe2170
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-07-12 16:39:42 +0530

    Added solution - LeetHub

---
+++ Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            int M = Integer.parseInt(S[0]);
+            int N = Integer.parseInt(S[1]);
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.primeRange(M, N);
+            for (int i : ans) System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Integer> primeRange(int M, int N) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+
+        return primes;
+    }
+}
