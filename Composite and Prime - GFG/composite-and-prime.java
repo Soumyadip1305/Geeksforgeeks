4f61cd788b28e23aefbb373f58d7d2caec67164f
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-07-14 13:07:35 +0530

    Added solution - LeetHub

--- Composite and Prime - GFG/composite-and-prime.java
+++ Composite and Prime - GFG/composite-and-prime.java
@@ -42,9 +42,6 @@ class Solution
     }
 
     private static boolean is_prime(long n) {
-        if (n == 1) {
-            return false;
-        }
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
