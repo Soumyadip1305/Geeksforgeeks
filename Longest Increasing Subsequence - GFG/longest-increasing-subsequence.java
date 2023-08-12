d17554ed471d4d21336c62535750547d3b218e22
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-08-12 09:04:04 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t > 0){
+            
+            //taking size of array
+            int n = sc.nextInt();
+            int array[] = new int[n];
+            
+            //inserting elements in the array
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+            
+            //creating an object of class Solution
+            Solution ob = new Solution();
+            
+            //calling longestSubsequence() method of class
+            //Solution
+            System.out.println(ob.longestSubsequence(n,array));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to find length of longest increasing subsequence.
+    static int longestSubsequence(int n, int arr[]) {
+        int dp[] = new int[n];
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(dp, 0, len - 1, arr[i]);
+            dp[idx] = arr[i];
+            if (idx == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    static int binarySearch(int arr[], int low, int high, int target) {
+        while (high >= low) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
