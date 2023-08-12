765126036d6c2f9451406b851e9f9725e59af28c
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-08-13 00:30:34 +0530

    Added solution - LeetHub

---
+++ Smallest number on left - GFG/smallest-number-on-left.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a1[] = in.readLine().trim().split("\\s+");
+            int a[] = new int[n];
+            for(int i = 0;i < n;i++)
+                a[i] = Integer.parseInt(a1[i]);
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.leftSmaller(n, a);
+            for(int i = 0;i < n;i++)
+                System.out.print(ans.get(i)+" ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static List<Integer> leftSmaller(int n, int heights[])
+    {
+       Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && stack.peek() >= heights[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                list.add(-1);
+            }
+            else {
+                list.add(stack.peek() );
+            }
+
+            stack.push(heights[i]);
+        }
+        return list;
+    }
+}
