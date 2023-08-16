7f1e5d9ff1f72bca809c39724b93a51c0f7a8c89
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-08-16 20:49:23 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.java
+++ Nth catalan number - GFG/nth-catalan-number.java
@@ -1,53 +1,44 @@
 //{ Driver Code Starts
 import java.io.*;
 import java.util.*;
-import java.math.*;
+
 class GFG {
-	public static void main (String[] args) {
-	    
-	    //taking input using Scanner class
-		Scanner sc=new Scanner(System.in);
-		
-		//taking total count of testcases
-		int t=sc.nextInt();
-		while(t-->0){
-		    
-		    //taking number of elements
-		    int n=sc.nextInt();
-		   
-		    //calling findCatalan() method
-		    System.out.println(new Solution().findCatalan(n));
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.findCatalan(n);
+            
+            System.out.println(res);
+            
+        }
+    }
 }
+
 // } Driver Code Ends
 
+class Solution {
+            public static int findCatalan(int n) {
+    if (n <= 1) {
+                return 1;
+            }
 
+            int mod = (int) 1e9 + 7;
+            int[] catalan = new int[n + 1];
+            catalan[0] = catalan[1] = 1;
 
-class Solution
-{
-    //Function to find the nth catalan number.
-    public static BigInteger findCatalan(int n)
-    {
-        BigInteger x = factorial(2 * n);
-        BigInteger y = factorial(n + 1).multiply(factorial(n));
-        return x.divide(y);
-    }
+            for (int i = 2; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    catalan[i] = (catalan[i] + (int)((long)catalan[j] * catalan[i - j - 1] % mod)) % mod;
+                }
+            }
 
-
-    public static BigInteger factorial(int n) {
-        BigInteger[] dp = new BigInteger[n + 1];
-        Arrays.fill(dp, BigInteger.valueOf(-1));
-        return factorialUtil(n, dp);
-    }
-
-    private static BigInteger factorialUtil(int n, BigInteger[] dp) {
-        if (n == 0) {
-            return BigInteger.ONE;
+            return catalan[n];
         }
-        if (!dp[n].equals(BigInteger.valueOf(-1))){
-            return dp[n];
         }
-           return dp[n] = BigInteger.valueOf(n).multiply(factorialUtil(n - 1, dp));
-    }
-}
