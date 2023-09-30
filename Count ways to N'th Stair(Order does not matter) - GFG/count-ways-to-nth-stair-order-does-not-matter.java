0c55b88dd812f257d71855efa3c54fdd93b442ab
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-09-30 21:13:13 +0530

    Added solution - LeetHub

---
+++ Count ways to N'th Stair(Order does not matter) - GFG/count-ways-to-nth-stair-order-does-not-matter.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            long ans = ob.nthStair(n);
+            System.out.println(ans);            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public long nthStair(int n){
+        return solve(n);
+    }
+        private static int solve(int n) {
+        if (n == 0) return 0;
+        int twoTake = 0;
+        int oneTake=0;
+        if (n > 2) {
+            twoTake = 1 + solve(n - 2);
+        }else {
+            oneTake = 1 + solve(n - 1);
+        }
+        return oneTake + twoTake;
+    }
+}
