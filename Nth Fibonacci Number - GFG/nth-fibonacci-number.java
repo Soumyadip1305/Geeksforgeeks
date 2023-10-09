d92e9299f898b4c8f6fe3495dfbd7c5fa98c92ba
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-10-09 21:41:37 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.java
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.java
@@ -22,18 +22,43 @@ class GFG {
 
 //User function Template for Java
 class Solution {
-    static int mod = 1000000007;
+    static int mod=1000000007;
+    static int nthFibonacci(int n){
+          //  int[] dp = new int[n + 1];
+        //  Arrays.fill(dp, -1);
+        //   System.out.println(nthFibo(n,dp));
+        //  System.out.println(nthFibo2(n));
+        return nthFibo3(n);
+    }
 
-    static int nthFibonacci(int n) {
-             if (n == 0) return 0;
-        if (n == 1) return 1;
-        int[] dp = new int[n+1];
 
+    static int nthFibo(int n, int[] dp) {
+        if (n <= 1) {
+            return n;
+        }
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = nthFibo(n - 1, dp) + nthFibo(n - 2, dp);
+    }
+
+    static int nthFibo2(int n) {  //   Tabulation
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = (int) ((dp[i - 1]%mod +dp[i - 2]%mod)%mod);
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+    static int nthFibo3(int n) {  //   Remove dp array
+        if (n == 0) return 0;
+        int prev = 1;
+        int prev2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int currI = (prev + prev2)%mod;
+            prev2 = prev;
+            prev = currI;
+        }
+        return  prev;
     }
 }
